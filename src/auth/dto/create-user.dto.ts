af977b05ab8749423a027dd692c79397fbd42312
erikaalexandrah <erika.hernandez@correo.unimet.edu.ve>
2025-11-09 19:17:50 -0400

    Revert "Add role field to user entity and registration"
    This reverts commit 5817befdc7585cc6509f344439b9e4488ad21083.

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,17 +1,14 @@
-import { IsString, Matches, IsOptional, IsIn } from 'class-validator';
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
-  username: string;
+  @MinLength(3)
+  @MaxLength(20)
+  username: string; 
 
-  @Matches(/^[0-9]{4,8}$/, {
-    message: 'La contraseña debe ser numérica de 4 a 8 dígitos',
-  })
-  password: string;
-
-  @IsOptional()
-  @IsIn(['admin', 'doctor', 'technician'], {
-    message: 'role debe ser admin, doctor o technician',
-  })
-  role?: string; // opcional (solo admin debería poder enviarlo)
+  @IsString()
+  @Matches(/^[0-9]+$/, { message: 'La contraseña debe ser solo números' })
+  @MinLength(4)
+  @MaxLength(8)
+  password: string; 
 }
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -5,17 +5,10 @@ import * as bcrypt from 'bcryptjs';
 @Schema()
 export class User extends Document {
   @Prop({ unique: true, required: true })
-  username: string;
+  username: string; // <--- nuevo campo
 
   @Prop({ required: true, select: false })
   password: string;
-
-  @Prop({ 
-    required: true, 
-    enum: ['admin', 'doctor', 'technician'],
-    default: 'technician'
-  })
-  role: string;
 
   @Prop({ default: true })
   isActive: boolean;
@@ -23,6 +16,7 @@ export class User extends Document {
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
+// Hash
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
